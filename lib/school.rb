3db312db8b05ab252939044cda835bd70804a131
Achsah Abraham <achsahmp@gmail.com>
2018-08-15 01:19:47 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,16 @@
-# code here!
+class School
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  def roster
+    @roster
+  end
+  def add_student(name, grade)
+    if @roster[grade] != nil
+    @roster[grade] << name
+  else
+    @roster[grade] = [name]
+  end
+end
